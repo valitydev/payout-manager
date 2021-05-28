bf7df987b4557c5dc410763fee347aa06ebfb5ea
karleowne <karleowne@gmail.com>
2021-05-28 17:29:34 +0700

    generate by rbk-templator

---
+++ src/main/java/com/rbkmoney/payoutmanager/PayoutManagerApplication.java
@@ -0,0 +1,15 @@
+package com.rbkmoney.payoutmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@ServletComponentScan
+@SpringBootApplication
+public class PayoutManagerApplication extends SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayoutManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rbkmoney/payoutmanager/PayoutManagerApplicationTest.java
@@ -0,0 +1,13 @@
+package com.rbkmoney.payoutmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = PayoutManagerApplication.class)
+public class PayoutManagerApplicationTest {
+
+    @Test
+    public void contextLoads() {
+
+    }
+}
