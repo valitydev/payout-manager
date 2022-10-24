f08ff09ddb3bfe0ab1cf373703f9326e3e762aab
Inal Arsanukaev <in.all.young@gmail.com>
2022-10-24 13:07:48 +0300

    TD-392: iam kafka (#22)
    * TD-392: iam kafka
    
    * TD-392: set concurrency property
    
    Co-authored-by: Inal Arsanukaev <aiz@empayre.com>

--- src/main/java/dev/vality/payout/manager/config/KafkaConfig.java
+++ src/main/java/dev/vality/payout/manager/config/KafkaConfig.java
@@ -3,18 +3,14 @@ package dev.vality.payout.manager.config;
 import dev.vality.kafka.common.serialization.ThriftSerializer;
 import dev.vality.machinegun.eventsink.SinkEvent;
 import dev.vality.payout.manager.Event;
-import dev.vality.payout.manager.config.properties.KafkaSslProperties;
 import dev.vality.payout.manager.serde.SinkEventDeserializer;
-import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.config.SslConfigs;
-import org.apache.kafka.common.security.auth.SecurityProtocol;
 import org.apache.kafka.common.serialization.Deserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
@@ -25,117 +21,65 @@ import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.listener.ContainerProperties;
 import org.springframework.kafka.listener.SeekToCurrentBatchErrorHandler;
 
-import java.io.File;
-import java.util.HashMap;
 import java.util.Map;
 
 import static org.apache.kafka.clients.consumer.OffsetResetStrategy.EARLIEST;
 
 @Configuration
-@EnableConfigurationProperties(KafkaSslProperties.class)
 public class KafkaConfig {
-
-    @Value("${kafka.bootstrap-servers}")
-    private String bootstrapServers;
-
-    @Value("${kafka.client-id}")
-    private String clientId;
-
-    @Value("${kafka.consumer.group-id}")
-    private String groupId;
-
-    @Value("${kafka.consumer.max-poll-interval-ms}")
-    private int maxPollInterval;
-
-    @Value("${kafka.consumer.max-session-timeout-ms}")
-    private int maxSessionTimeout;
-
-    @Value("${kafka.topic.source.consume.max-poll-records}")
-    private String sourceMaxPollRecords;
 
     @Value("${kafka.topic.source.consume.concurrency}")
     private int sourceConcurrency;
 
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, SinkEvent> sourceContainerFactory(
-            KafkaSslProperties kafkaSslProperties) {
+            KafkaProperties kafkaProperties) {
         var containerFactory = new ConcurrentKafkaListenerContainerFactory<String, SinkEvent>();
         configureContainerFactory(
                 containerFactory,
                 new SinkEventDeserializer(),
-                clientId + "-source",
-                sourceMaxPollRecords,
-                kafkaSslProperties);
+                kafkaProperties);
         containerFactory.setConcurrency(sourceConcurrency);
         return containerFactory;
     }
 
     @Bean
-    public KafkaTemplate<String, Event> kafkaTemplate(KafkaSslProperties kafkaSslProperties) {
-        return new KafkaTemplate<>(producerFactory(kafkaSslProperties));
+    public KafkaTemplate<String, Event> kafkaTemplate(KafkaProperties kafkaProperties) {
+        return new KafkaTemplate<>(producerFactory(kafkaProperties));
     }
 
     private <T> void configureContainerFactory(
             ConcurrentKafkaListenerContainerFactory<String, T> containerFactory,
             Deserializer<T> deserializer,
-            String clientId,
-            String maxPollRecords,
-            KafkaSslProperties kafkaSslProperties) {
+            KafkaProperties kafkaProperties) {
         var consumerFactory = createKafkaConsumerFactory(
                 deserializer,
-                clientId,
-                maxPollRecords,
-                kafkaSslProperties);
+                kafkaProperties);
         containerFactory.setConsumerFactory(consumerFactory);
         containerFactory.setBatchErrorHandler(new SeekToCurrentBatchErrorHandler());
         containerFactory.setBatchListener(true);
         containerFactory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
     }
 
-    private ProducerFactory<String, Event> producerFactory(KafkaSslProperties kafkaSslProperties) {
-        Map<String, Object> config = new HashMap<>();
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+    private ProducerFactory<String, Event> producerFactory(KafkaProperties kafkaProperties) {
+        Map<String, Object> config = kafkaProperties.buildProducerProperties();
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, ThriftSerializer.class);
-        configureSsl(config, kafkaSslProperties);
         return new DefaultKafkaProducerFactory<>(config);
     }
 
     private <T> DefaultKafkaConsumerFactory<String, T> createKafkaConsumerFactory(
             Deserializer<T> deserializer,
-            String clientId,
-            String maxPollRecords,
-            KafkaSslProperties kafkaSslProperties) {
-        Map<String, Object> properties = defaultProperties(kafkaSslProperties);
-        properties.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
-        properties.put(ConsumerConfig.CLIENT_ID_CONFIG, clientId);
-        properties.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, maxPollRecords);
+            KafkaProperties kafkaProperties) {
+        Map<String, Object> properties = defaultProperties(kafkaProperties);
         return new DefaultKafkaConsumerFactory<>(properties, new StringDeserializer(), deserializer);
     }
 
-    private Map<String, Object> defaultProperties(KafkaSslProperties kafkaSslProperties) {
-        Map<String, Object> properties = new HashMap<>();
-        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+    private Map<String, Object> defaultProperties(KafkaProperties kafkaProperties) {
+        Map<String, Object> properties = kafkaProperties.buildConsumerProperties();
+        properties.put(ConsumerConfig.CLIENT_ID_CONFIG, kafkaProperties.getConsumer().getClientId() + "-source");
         properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, EARLIEST.name().toLowerCase());
         properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
-        properties.put(ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG, maxPollInterval);
-        properties.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, maxSessionTimeout);
-        configureSsl(properties, kafkaSslProperties);
         return properties;
-    }
-
-    private void configureSsl(Map<String, Object> properties, KafkaSslProperties kafkaSslProperties) {
-        if (kafkaSslProperties.isEnabled()) {
-            properties.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, SecurityProtocol.SSL.name());
-            properties.put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG,
-                    new File(kafkaSslProperties.getTrustStoreLocation()).getAbsolutePath());
-            properties.put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, kafkaSslProperties.getTrustStorePassword());
-            properties.put(SslConfigs.SSL_KEYSTORE_TYPE_CONFIG, kafkaSslProperties.getKeyStoreType());
-            properties.put(SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG, kafkaSslProperties.getTrustStoreType());
-            properties.put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG,
-                    new File(kafkaSslProperties.getKeyStoreLocation()).getAbsolutePath());
-            properties.put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, kafkaSslProperties.getKeyStorePassword());
-            properties.put(SslConfigs.SSL_KEY_PASSWORD_CONFIG, kafkaSslProperties.getKeyPassword());
-        }
     }
 }
